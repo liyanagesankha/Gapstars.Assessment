129e7294cf5f791733cc5efe8c5242b65a55750d
Sankha H Liyanage <s.h.liyanage@outlook.com>
2022-12-19 04:28:26 +0530

    Add service within scope life time

--- Chinook/Program.cs
+++ Chinook/Program.cs
@@ -1,6 +1,7 @@
 using Chinook;
 using Chinook.Areas.Identity;
 using Chinook.Models;
+using Chinook.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +20,11 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ChinookUser>>();
 
 builder.Services.AddSingleton<StateContainer>();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserPlayListService, UserPlayListService>();
+builder.Services.AddScoped<IArtistService, ArtistService>();
+builder.Services.AddScoped<ITrackService, TrackService>();
 
 var app = builder.Build();
 
