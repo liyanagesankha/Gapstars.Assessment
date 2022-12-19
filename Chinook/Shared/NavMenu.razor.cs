8c564da4b24434b1fe605c0b2b76645f5bbd225c
Sankha H Liyanage <s.h.liyanage@outlook.com>
2022-12-19 05:54:44 +0530

    Update left navbar functionality.

--- Chinook/Shared/NavMenu.razor.cs
+++ Chinook/Shared/NavMenu.razor.cs
@@ -1,6 +1,27 @@
-﻿namespace Chinook.Shared
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Chinook.Shared
 {
-    public partial class NavMenu
+    public partial class NavMenu : ComponentBase, IDisposable
     {
+        [Inject] StateContainer StateContainer { get; set; }
+
+        private bool collapseNavMenu = true;
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        protected override async Task OnInitializedAsync()
+        {
+            StateContainer.OnChange += StateHasChanged;
+        }
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        public void Dispose()
+        {
+            StateContainer.OnChange -= StateHasChanged;
+        }
     }
 }
